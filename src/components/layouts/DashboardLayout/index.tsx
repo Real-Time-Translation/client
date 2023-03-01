5c0acc310a171f79d469c512092c22c19ed53b86
evgeny <afazhenya2001@gmail.com>
2023-03-01 23:24:28 +0300

    small fix

--- src/components/layouts/DashboardLayout/index.tsx
+++ src/components/layouts/DashboardLayout/index.tsx
@@ -5,13 +5,17 @@ import { useStyles } from './index.styles';
 /** Wrapper for Dashboard-like containers
  * Sets minimum page height configurations and backgrounds
  * */
-export const DashboardLayout: FC<DashboardProps> = ({ children, header, footer }) => {
+export const DashboardLayout: FC<DashboardProps> = ({
+  children,
+  header,
+  footer,
+}) => {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <header>{header}</header>
-      <body className={classes.body}>{children}</body>
+      <div className={classes.body}>{children}</div>
       <footer>{footer}</footer>
     </div>
   );
