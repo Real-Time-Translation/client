5f15d1f29855e96faa6313c179671824deb43bd2
evgeny <afazhenya2001@gmail.com>
2023-03-26 20:43:21 +0300

    add build

--- src/api/socket.ts
+++ src/api/socket.ts
@@ -1,8 +1,8 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:8080');
+const socket = io(import.meta.env.VITE_SERVER_HOST);
 socket.on('connect', () => {
-  console.log('Connected to ws dev server: http://localhost:8080');
+  console.log('Connected to ws dev server:', import.meta.env.VITE_SERVER_HOST);
 });
 
 export default socket;
