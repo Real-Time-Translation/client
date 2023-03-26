bb923c60b42a9f813c69ac8dbee2bcd2075904e8
evgeny <afazhenya2001@gmail.com>
2023-03-26 23:03:16 +0300

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   base: '/client/',
+  server: {
+    https: false,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
